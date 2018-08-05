b21608d6a37d9b7d99d3ba1a2dd696c3117c8e1c
Benoît <benoit.garcon@outlook.com>
2018-08-05 15:34:52 +0200

    test 10

--- js/total-data.js
+++ js/total-data.js
@@ -5,9 +5,22 @@ var loadJSON = function(file) {
         response = json;
         console.log(response);
     });
-    console.log(response);
     return response;
 }
 
-loadJSON('../data/projects-2018-08-05.json');
-loadJSON('data/projects-2018-08-05.json');
+var loadFile = function(file) {
+    var response = undefined;
+    $.get(file, function(content) {
+        response = content;
+        console.log(response);
+    });
+    return response;
+}
+
+
+var latestData = loadFile('../data/latest');
+var projects = loadJSON('../data/'+latestData);
+
+$("#projects_count").text(function(index, old) {
+    return projects.length;
+});
